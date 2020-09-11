e1adeb6a8e52eb07a6e05db76646caf5879dc531
harshinithiyagarajan <68841703+harshinithiyagarajan@users.noreply.github.com>
2020-09-11 15:42:33 +0530

    Add files via upload

---
+++ equalparts.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labexercise8;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Thiagarajan
+ */
+public class equalparts {
+    public  static void main(String[] args){
+        Scanner obj=new Scanner(System.in);
+        String s="aaabbbcccdddeee";
+        System.out.println("string:"+s);
+        String b="";
+        int len=s.length();
+        System.out.println("enter no of parts:");
+        int n=obj.nextInt();
+        int c=len/n;
+        if(len%n!=0)
+            System.out.println("the given string cannot be divided into equal parts");
+        else
+        {
+            for(int i=0;i<len;i=i+c)
+            {
+                for(int j=0;j<c;j++)
+                {
+                    b=b+s.charAt(i);
+                }
+                 b=b+" ";
+            }
+             System.out.println(b);
+        }
+       
+    }
+    
+}
